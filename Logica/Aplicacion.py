2bfab0f71a8561ac2a63334a5b9a8dd7bab3219e
marialebu <mabu12@outlook.com>
2015-10-24 02:43:56 -0500

    fghjklñ
    gbjknmk

--- Logica/Aplicacion.py
+++ Logica/Aplicacion.py
@@ -11,11 +11,7 @@ from sqlalchemy.sql import table, column, select, update, insert, and_
 
 class Aplicacion():
     def __init__(self):
-        self.add=Agregar()
-    def agregaPaciente(self, idP, ti, name, last, gender, birth, age, phone):
-        self.add.agregaPaciente(idP, ti, name, last, gender, datetime.datetime.strptime(birth, "%d %b %y").date(), age, phone)
+        pass
 
 if __name__=="__main__":
     apl=Aplicacion()
-    apl.agregaPaciente("1014270644", "CC", "Alejandro", "Negro", "F", "05 Oct 95", 20, "3122984750")
-#add.agregaPaciente("1014270644", "CC", "Alejandro", "Negro", "F", datetime.datetime.strptime("05 Oct 95", "%d %b %y").date(), 20, "3122984750")
--- Vista/MainFile.py
+++ Vista/MainFile.py
@@ -1,23 +1,48 @@
 from sqlalchemy.sql.base import _from_objects
-
 __author__ = 'MariaAlejandraLuisFelipe'
 from PyQt5 import QtCore, QtGui, QtWidgets
 from Vista.agregarpacienteGUI import Ui_MainWindow
 from Logica.Aplicacion import *
+from Persistencia.Base import *
 
 class MainWindows(QtWidgets.QMainWindow):
-    tids=["CC", "CE", "TI", "Registro Civil"]
+    tids, gender=["CC", "CE", "TI", "Registro Civil"], ["F", "M", "Otro"]
+    month=["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
     def __init__(self, *args, **kwargs):
         super(MainWindows, self).__init__(*args, **kwargs)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        self.ui.ti.
-        self.ui.send.clicked.connect(self.enviarPaciente())
+        self.ui.ti.addItems(self.tids)
+        self.ui.gender.addItems(self.gender)
+        self.ui.send.clicked.connect(self.enviarPaciente)
+        self.add=Agregar()
 
     def enviarPaciente(self):
-        id=self.ui.idp.toPlainText()
-        Aplicacion.agregaPaciente()
+        idP, ti=self.ui.idp.toPlainText(), self.tids[self.ui.ti.currentIndex()]
+        name, last=self.ui.name.toPlainText(), self.ui.last.toPlainText()
+        gender, b=self.gender[self.ui.gender.currentIndex()], self.ui.dateEdit.date()
+        birth=""
+        if(b.day()<10):
+            birth+="0"+str(b.day())
+        else:
+            birth+=str(b.day())
+        if (b.month()<10):
+            birth+="/"+"0"+str(b.month())
+        else:
+            birth+="/"+str(b.month())
+        birth+="/"+str(b.year())[2:4] ; print(birth)
+        age=self.ui.spinBox.value(); phone=self.ui.phone.toPlainText()
+        if(not(self.validaDatos(idP, name, last, age, phone))):
+            raise Exception("ERROR DATOS CORRUPTOS")
+        self.add.agregaPaciente(idP, ti, name, last, gender, datetime.datetime.strptime(birth, "%d/%m/%y").date(), age, phone)
 
+    def validaDatos(self, idP, name, last, age, phone):
+        r=True
+        try:
+            int(idP) ; r=len(name.strip())>0 and len(last.strip())>0 and age>=0 and len(phone.strip())>0 ; int(phone)
+            print(r)
+        except:
+            raise Exception("Datos corruptos")
 
 if __name__=="__main__":
     import sys
