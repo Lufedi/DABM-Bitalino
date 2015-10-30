b7f6a834b68df67aad6ca4b1838b8ef0b259d750
Lufedi <pipexir@gmail.com>
2015-10-29 19:05:47 -0500

    Agregando la clase Bitalino funcionando en 3.4 y el GUI de buscar dispositivos con bluetooh

---
+++ Logica/Adaptador.py
@@ -0,0 +1,22 @@
+from Logica import bitalino
+
+__author__ = 'Pipe'
+
+
+
+from Logica import *
+class Adaptador(object):
+
+    def encontrarDispositivos(self):
+        dispositivos =  bitalino.find()
+        for disp in dispositivos:
+            print(disp)
+        return  dispositivos
+    #retorna el dispositivo ya conectado
+    def conectarseADispositivo(self, mac):
+        device = bitalino.BITalino(mac)
+        device.start(1000, [0])
+        print(device.read(15))
+        return device
+#a = Adaptador()
+#a.conectarseADispositivo("FC:A1:3E:B7:37:FD")
--- Vista/MainFile.py
+++ Vista/MainFile.py
@@ -2,8 +2,10 @@ from sqlalchemy.sql.base import _from_objects
 __author__ = 'MariaAlejandraLuisFelipe'
 from PyQt5 import QtCore, QtGui, QtWidgets
 from Vista.agregarpacienteGUI import Ui_MainWindow
+from Vista.buscarDispositivosGUI import Ui_MainWindow2
 from Logica.Aplicacion import *
 from Persistencia.Base import *
+from Logica.Adaptador import *
 
 class MainWindows(QtWidgets.QMainWindow):
     tids, gender=["CC", "CE", "TI", "Registro Civil"], ["F", "M", "Otro"]
@@ -47,10 +49,42 @@ class MainWindows(QtWidgets.QMainWindow):
         self.ui.idp.setText("")
         self.ui.name.setText(""), self.ui.last.setText("")
 
+class TarjetaBitalinoWindow(QtWidgets.QMainWindow):
+    def __init__(self, *args, **kwargs):
+        super(TarjetaBitalinoWindow, self).__init__(*args, **kwargs)
+        self.ui = Ui_MainWindow2()
+        self.ui.setupUi(self)
+        self.ui.pushButton.clicked.connect(self.buscarDispositivos)
+        self.ui.conectar.clicked.connect(self.conectar)
+        self.adaptador = Adaptador()
+
+
+    def buscarDispositivos(self):
+        print("Buscando")
+        l = self.adaptador.encontrarDispositivos()
+        print(l)
+        for d in l:
+            self.ui.comboBox.addItem(str(d[0])+"-"+str(d[1]))
+
+    def conectar(self):
+        print("Conectando")
+        mac  = (self.ui.comboBox.currentText()).split("-")[0]
+        print(mac)
+        dispositivo = self.adaptador.conectarseADispositivo(mac)
+
+'''def v1():
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    pp = TarjetaBitalinoWindow()
+    pp.show()
+    sys.exit(app.exec())
+v1()'''
 if __name__=="__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
     pp = MainWindows()
     pp.show()
     sys.exit(app.exec_())
+
+
 
